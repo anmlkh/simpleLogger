00437687c53e41596ed168cd5e7141fadc2ca2b8
Andrii Malykhin <andrii.malykhin@omio.com>
2023-04-13 15:32:25 +0200

    project init

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+const app = express();
+
+// Use the body-parser middleware to parse request bodies
+app.use(bodyParser.json());
+
+// Use the morgan middleware to log requests
+app.use(morgan('dev'));
+
+// Create a route for handling post requests
+app.post('/api/log', (req, res) => {
+  console.log(req.body);
+  res.send('Post received');
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
